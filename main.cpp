e6c0424970b60bbeb980b2c15e26e90a3e29b3e0
孟轩羽 <mengxy5236@126.com>
2025-05-27 15:59:40 +0800

    01knapsack527

--- main.cpp
+++ main.cpp
@@ -6,17 +6,39 @@
 using namespace std;
 
 int main() {
-    // 示例数据（P01）
-    int capacity = 165;
-    vector<int> weights = { 23, 31, 29, 44, 53, 38, 63, 85, 89, 82 };
-    vector<int> profits = { 92, 57, 49, 68, 60, 43, 67, 84, 87, 72 };
+    // 示例数据（P0X）
+    int capacity = 0;
+    vector<int> weights ;
+    vector<int> profits ;
+	int n = 0;// 每组对象的数量
+
+	cout << "输入背包的容量与每组对象的数量(用空格分隔)：" << endl;
+    cin >> capacity >> n;
+
+    if (capacity < 0 || n <= 0) {
+        cerr << "输入的容量或对象数量不合法。" << endl;
+        return 1;
+    }
+
+	cout << "输入每个对象的权重（用空格分隔）：" << endl;
+	for (int i = 0; i < n; ++i) {
+		int weight;
+		cin >> weight;
+		weights.push_back(weight);
+	}
+	cout << "输入每个对象的利润（用空格分隔）：" << endl;
+	for (int i = 0; i < n; ++i) {
+		int profit;
+		cin >> profit;
+		profits.push_back(profit);
+	}
 
     pair<int, vector<int>> result = knapsackGreedy(capacity, weights, profits);
     int value = result.first;
     vector<int> selection = result.second;
 
     cout << "总价值: " << value << endl;
-    cout << "选择向量: ";
+    cout << "权重的最优选择: ";
     for (int sel : selection) {
         cout << sel << " ";
     }
